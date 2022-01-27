8def7aff0e0a20a5e82adb32c2522e8c532c54d7
Lovejot Singh <lovejot.saini1711@gmail.com>
2022-01-27 15:48:03 +0530

    integrating keycloak

--- src/App.js
+++ src/App.js
@@ -1,25 +1,41 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from 'react'
+import Keycloak from 'keycloak-js'
+import axiosInstance from './intercept'
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class Secured extends Component {
+
+  constructor(props) {
+    super(props);
+    this.state = { keycloak: null, authenticated: false }
+  }
+
+  componentDidMount() {
+    const keycloak =Keycloak('/keycloak.json');
+    keycloak.init({onLoad:'login-required'}).then(authenticated =>{
+      this.setState({keycloak:keycloak,authenticated:authenticated})
+      if(authenticated){
+       console.log('token',keycloak.token)
+        window.accessToken=keycloak.token
+      }
+    })
+  }
+
+
+  render() {
+    if(this.state.keycloak){
+      if(this.state.authenticated) return (
+        <div>
+          <p>this is a secured component of your app</p>
+      
+        </div>
+      ); else return ( <div>unable to authenticate!</div>)
+    }
+    return (
+      <div>
+        <h1>initializing keycloak</h1>
+    
+      </div>
+    )
+  }
 }
-
-export default App;
+export default Secured
